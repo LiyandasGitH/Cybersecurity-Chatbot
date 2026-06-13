fbab39d53be9d02b7c2744fd2c627458ba498107
liyanda <litoncpt025@student.wethinkcode.co.za>
2026-06-13 11:53:51 +0200

    feat: added draft for uses

--- src/main/java/com/cyberchatbot/Main.java
+++ src/main/java/com/cyberchatbot/Main.java
@@ -1,10 +1,22 @@
 package com.cyberchatbot;
 
+//import org.fusesource.jansi.AnsiConsole;
 
+import java.util.Scanner;
+
+import static com.cyberchatbot.ConsoleUI.printPrompt;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         ConsoleUI.printBanner();
+        // future implementation for Windows terminal display
+//        AnsiConsole.systemInstall();
+        VoiceGreeter.greet();
 
+        Chatbot bot = new Chatbot();
+        bot.start();
+
+//        String user = prompt(scanner);
     }
 }
