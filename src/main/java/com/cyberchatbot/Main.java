8945e63b4d7c6a79d9fae95a1aac5557d275e0dd
liyanda <litoncpt025@student.wethinkcode.co.za>
2026-06-20 10:29:13 +0200

    feat/refactor: moved the main conversation loops into Chatbot, ckearing up Main for Single Responsibility Principle

--- src/main/java/com/cyberchatbot/Main.java
+++ src/main/java/com/cyberchatbot/Main.java
@@ -9,56 +9,16 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+//        Scanner scanner = new Scanner(System.in);
         ConsoleUI.printBanner();
         VoiceGreeter.greet();
-
-        String userName = "";
-        while (userName.isEmpty()) {
-            ConsoleUI.printBotResponse("Hello! Before we begin, what is your name?");
-            ConsoleUI.printPrompt();
-            userName = scanner.nextLine().trim();
-
-            if (userName.isEmpty()) {
-                ConsoleUI.printBotResponse("Name cannot be empty. Please enter a valid name.");
-                System.out.println();
-            }
-            // checks if name entered is "quit" or "exit"
-            else if (userName.equalsIgnoreCase("exit") || (userName.equalsIgnoreCase("quit"))) {
-//                System.out.println();
-                ConsoleUI.printBotResponse("Goodbye " + userName + "! Stay safe online!");
-                return;
-            }
-
-        }
-        ConsoleUI.printBotResponse("Hi " + userName + "! Ask me about phishing, password and malware attacks.");
 
 //         future implementation for Windows terminal display
 //        AnsiConsole.systemInstall();
 
+        Chatbot bot = new Chatbot();
+        bot.start();
 
-//        Chatbot bot = new Chatbot();
-//        bot.start();
-//
-        boolean isActive = true;
-        while (isActive) {
-            ConsoleUI.printPrompt();
-            String userInput = scanner.nextLine().trim();
-
-            if (userInput.equalsIgnoreCase("exit") || (userInput.equalsIgnoreCase("quit"))) {
-//                System.out.println();
-                ConsoleUI.printBotResponse("Goodbye " + userName + "! Stay safe online!");
-                break;
-            }
-
-            // placeholder for bot response
-            ResponseEngine.getResponse(userInput);
-
-//            else {
-//                isActive = false;
-//            }
-
-        }
-        scanner.close();
+//        scanner.close();
     }
 }
--- src/main/java/com/cyberchatbot/chatbot/Chatbot.java
+++ src/main/java/com/cyberchatbot/chatbot/Chatbot.java
@@ -1,7 +1,79 @@
 package com.cyberchatbot.chatbot;
 
+import com.cyberchatbot.ui.ConsoleUI;
+
+import java.util.Scanner;
+
 public class Chatbot {
 
-    public void start() {
+    private User user;
+    private final ResponseEngine responseEngine;
+    private final Scanner scanner;
+
+    public Chatbot() {
+        this.responseEngine = new ResponseEngine();
+        this.scanner = new Scanner(System.in);
     }
+
+    public void start() {
+
+        if (!fetchUserName()) {
+            return;
+        }
+
+        ConsoleUI.printBotResponse(
+                "Hi " + user.getName() + "! I can answer questions about " +
+                responseEngine.getTopicCount() + " cybersecurity topics. " +
+                "\n" + "Type 'exit' to quit program."
+        );
+        chatWithBot();
+    }
+
+    private boolean fetchUserName() {
+        String userName = "";
+
+        while (userName.isEmpty()) {
+            ConsoleUI.printBotResponse("Hello! Before we begin, what is your name?");
+            ConsoleUI.printPrompt();
+            userName = scanner.nextLine().trim();
+
+            if (userName.isEmpty()) {
+                ConsoleUI.printError("Name cannot be empty. Please enter a valid name.");
+                System.out.println();
+            }
+            // checks if name entered is "quit" or "exit"
+            else if (userName.equalsIgnoreCase("exit") || (userName.equalsIgnoreCase("quit"))) {
+                goodbye();
+                return false;
+            }
+        }
+        this.user = new User(userName);
+        return true;
+    }
+
+    private void chatWithBot() {
+        boolean isActive = true;
+        while (isActive) {
+            ConsoleUI.printPrompt();
+            String userInput = scanner.nextLine();
+
+            if (userInput == null || userInput.trim().isEmpty()) {
+                ConsoleUI.printError("Please type a valid question or topic.");
+            }
+            else if (userInput.equalsIgnoreCase("exit") || (userInput.equalsIgnoreCase("quit"))) {
+                goodbye();
+                break;
+            } else {
+                String botAnswer = responseEngine.getResponse(userInput);
+                ConsoleUI.printBotResponse(botAnswer);
+            }
+        }
+    }
+
+    private void goodbye() {
+        System.out.println();
+        String nameGotten = (user != null) ? user.getName() : "User";
+        ConsoleUI.printBotResponse("Goodbye " + nameGotten + "! Stay safe online!");
+    }
+
 }
