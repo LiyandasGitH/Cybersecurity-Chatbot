c645a5b46132bd01f13c23d2ab34c0598a99db88
liyanda <litoncpt025@student.wethinkcode.co.za>
2026-06-29 13:06:25 +0200

    test: started Chatbot tests

--- src/test/java/com/cyberchatbot/chatbot/ChatbotTest.java
+++ src/test/java/com/cyberchatbot/chatbot/ChatbotTest.java
@@ -1,4 +1,64 @@
 package com.cyberchatbot.chatbot;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ChatbotTest {
+
+    private final InputStream originalSystemIn = System.in;
+
+    @AfterEach
+    void tearDown() {
+        System.setIn(originalSystemIn);
+    }
+
+    private void simulateUserInput(String data) {
+        ByteArrayInputStream testIn = new ByteArrayInputStream(data.getBytes());
+        System.setIn(testIn);
+    }
+
+    @Test
+    void testStartAndExitAtNamePrompt() {
+        simulateUserInput("exit\n");
+
+        Chatbot chatbot = new Chatbot();
+
+        assertDoesNotThrow(chatbot::start);
+    }
+
+    @Test
+    void testSuccessfulLoginAndExit() {
+        String inputs = "Ink\nquit\n";
+        simulateUserInput(inputs);
+
+        Chatbot chatbot = new Chatbot();
+
+        assertDoesNotThrow(chatbot::start);
+    }
+
+    @Test
+    void testNameValidationHandlesEmptyInput() {
+        String inputs = "\n     \nquit\n";
+        simulateUserInput(inputs);
+
+        Chatbot chatbot = new Chatbot();
+
+        assertDoesNotThrow(chatbot::start);
+    }
+
+    @Test
+    void testChatbotExecutesValidKeywordQueries() {
+        String inputs = "Ink\npasswords\n\nexit\n";
+        simulateUserInput(inputs);
+
+        Chatbot chatbot = new Chatbot();
+
+        assertDoesNotThrow(chatbot::start);
+    }
 }
